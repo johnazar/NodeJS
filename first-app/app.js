ef6c7f47c4d0cacba245de75c855330f7f282cd7
John Azar <eng.johnazar@gmail.com>
2020-11-03 22:50:47 +0300

    built-in module

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,7 @@
 // Import module
-const logger = require ('./logger');
+const logger = require ('./log_module');
+// Built in module
+const path = require('path');
 
 
 function sayHello(name)
@@ -7,9 +9,14 @@ function sayHello(name)
     console.log('Hello ' + name);
     logger.logtocloud(name);
 }
-sayHello('John');
+//sayHello('John');
 //console.log(logger);
 //console.log(global);
 //console.log(module);
-console.log(__filename);
-//console.log(__dirname);
+console.log('__filename ',__filename);
+console.log('__dirname ', __dirname);
+
+
+// work with path module
+var pathObj = path.parse(__filename);
+console.log(pathObj);
