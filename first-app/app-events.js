802c83f8075255ebebb4fdd33a3a2bdb66a5d6d8
John Azar <eng.johnazar@gmail.com>
2020-11-03 23:27:07 +0300

    Events argsuments

--- first-app/app-events.js
+++ first-app/app-events.js
@@ -2,10 +2,10 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged',function(){
-    console.log('messageLogged');
+emitter.on('messageLogged',(arg)=>{ // arg OR e , as you like
+    console.log('Listener called ', arg);
 });
 //Rise an event
-emitter.emit('messageLogged');
-
+emitter.emit('messageLogged',{id:1,url:'http://'});
+//Rise an event
 
